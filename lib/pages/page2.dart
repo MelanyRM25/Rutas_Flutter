b4b6d4a7d5220f19618b66e1b5e8677ddb546e87
unknown <melanyrm2507@gmail.com>
2024-10-17 02:38:44 -0400

    Agregamos Navigator Pop

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -8,13 +8,23 @@ class Page2 extends StatefulWidget {
 }
 
 class _Page2State extends State<Page2> {
+  void irPagina1() {
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Page 2'),
       ),
-      body: const Center(),
+      body: Center(
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurpleAccent.shade100),
+            onPressed: irPagina1,
+            child: Text("Atras")),
+      ),
     );
   }
 }
